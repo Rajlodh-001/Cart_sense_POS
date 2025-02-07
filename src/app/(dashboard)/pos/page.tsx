3f1544d67e14adbd6d737f2d92187085f9d1ca2c
Rajlodh-001 <Rajlodh911@gmail.com>
2025-02-07 15:46:33 +0530

    pagelayout

--- src/app/(dashboard)/pos/page.tsx
+++ src/app/(dashboard)/pos/page.tsx
@@ -1,12 +1,13 @@
  const PosPage =()=>{
     return(
         <div className="h-screen flex">
-        {/* Left */}
-        <div className="w-[50%] bg-blue-200">
-    
+      {/* Left */}
+      <div className="w-[70%] md:w-[65%] lg:w-[75%] bg-blue-200">
+            {/* <PosPage/> */}
+
         </div>
         {/* Right */}
-        <div className="w-[50%] bg-red-200">dfsd</div>
+        <div className="w-[30%] md:w-[35%] lg:w-[25%] bg-red-200"></div>
     
     </div>
     )
