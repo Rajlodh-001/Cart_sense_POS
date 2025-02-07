cdd35378c997730e58b3e003c34a8d91c33adf18
Rajlodh-001 <Rajlodh911@gmail.com>
2025-02-07 15:37:00 +0530

    application page

---
+++ src/app/(dashboard)/pos/page.tsx
@@ -0,0 +1,17 @@
+ const PosPage =()=>{
+    return(
+        <div className="h-screen flex">
+        {/* Left */}
+        <div className="w-[50%] bg-blue-200">
+    
+        </div>
+        {/* Right */}
+        <div className="w-[50%] bg-red-200">dfsd</div>
+    
+    </div>
+    )
+
+
+}
+
+export default PosPage
