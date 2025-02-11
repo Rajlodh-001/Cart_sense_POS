cd4fd0362d85733803a14b3c30348e82ba68ddf5
Rajlodh-001 <Rajlodh911@gmail.com>
2025-02-11 22:28:41 +0530

    Added Menupage

---
+++ src/components/MenuPage.tsx
@@ -0,0 +1,123 @@
+import Image from "next/image";
+
+const MenuPage = () => {
+  return (
+    <>
+      <div className="w-full m-2  ">
+        <div className=" grid bg-bgdarkgray xl:grid-cols-5  lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2">
+
+          <div className=" bg-white rounded-md">
+            <div className="flex flex-col">
+              <div className="p-2">
+                <div className="h-[80px] w-[120px] flex items-center rounded-md bg-gray-400  "></div>
+              </div>
+              <p className="font-medium p-2  text-xl">Veg Sandwhich</p>
+              <div className="flex flex-row">
+                
+              <p className=" text-xs m-2  bg-orange-50 rounded-full  text-orange-400">sandwich</p>
+              <div className="text-2xl pl-9 p-2">$6.49</div>
+              </div>
+            </div>
+
+            {/* <Image src={"./sfsf"}/> */}
+
+            
+          </div>
+
+          <div className=" bg-white rounded-md">
+            <div className="flex flex-col">
+              <div className="p-2">
+                <div className="h-[80px] w-[120px] flex items-center rounded-md bg-gray-400  "></div>
+              </div>
+              <p className="font-medium p-2  text-xl">Veg Sandwhich</p>
+              <div className="flex flex-row">
+                
+              <p className=" text-xs m-2  bg-orange-50 rounded-full  text-orange-400">sandwich</p>
+              <div className="text-2xl pl-9 p-2">$6.49</div>
+              </div>
+            </div>
+            
+            {/* <Image src={"./sfsf"}/> */}
+
+            
+          </div>
+
+          <div className=" bg-white rounded-md">
+            <div className="flex flex-col">
+              <div className="p-2">
+                <div className="h-[80px] w-[120px] flex items-center rounded-md bg-gray-400  "></div>
+              </div>
+              <p className="font-medium p-2  text-xl">Veg Sandwhich</p>
+              <div className="flex flex-row">
+                
+              <p className=" text-xs m-2  bg-orange-50 rounded-full  text-orange-400">sandwich</p>
+              <div className="text-2xl pl-9 p-2">$6.49</div>
+              </div>
+            </div>
+            
+            {/* <Image src={"./sfsf"}/> */}
+
+            
+          </div>
+
+          <div className=" bg-white rounded-md">
+            <div className="flex flex-col">
+              <div className="p-2">
+                <div className="h-[80px] w-[120px] flex items-center rounded-md bg-gray-400  "></div>
+              </div>
+              <p className="font-medium p-2  text-xl">Veg Sandwhich</p>
+              <div className="flex flex-row">
+                
+              <p className=" text-xs m-2  bg-orange-50 rounded-full  text-orange-400">sandwich</p>
+              <div className="text-2xl pl-9 p-2">$6.49</div>
+              </div>
+            </div>
+            
+            {/* <Image src={"./sfsf"}/> */}
+
+            
+          </div>
+
+          <div className=" bg-white rounded-md">
+            <div className="flex flex-col">
+              <div className="p-2">
+                <div className="h-[80px] w-[120px] flex items-center rounded-md bg-gray-400  "></div>
+              </div>
+              <p className="font-medium p-2  text-xl">Veg Sandwhich</p>
+              <div className="flex flex-row">
+                
+              <p className=" text-xs m-2  bg-orange-50 rounded-full  text-orange-400">sandwich</p>
+              <div className="text-2xl pl-9 p-2">$6.49</div>
+              </div>
+            </div>
+            
+            {/* <Image src={"./sfsf"}/> */}
+
+            
+          </div>
+
+          <div className=" bg-white rounded-md">
+            <div className="flex flex-col">
+              <div className="p-2">
+                <div className="h-[80px] w-[120px] flex items-center rounded-md bg-gray-400  "></div>
+              </div>
+              <p className="font-medium p-2  text-xl">Veg Sandwhich</p>
+              <div className="flex flex-row">
+                
+              <p className=" text-xs m-2  bg-orange-50 rounded-full  text-orange-400">sandwich</p>
+              <div className="text-2xl pl-9 p-2">$6.49</div>
+              </div>
+            </div>
+            
+            {/* <Image src={"./sfsf"}/> */}
+
+            
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MenuPage;
