dfe0dd8f8c20cdb4fbc7e56c72d05c2245028f69
Rajlodh-001 <Rajlodh911@gmail.com>
2025-02-11 22:21:35 +0530

    Added submenu bar

---
+++ src/components/MiniMenuNavbar.tsx
@@ -0,0 +1,78 @@
+const MiniMenuNavbar = () => {
+  return (
+    <>
+      <div className="w-full m-2  ">
+        <div className="grid grid-cols-5 gap-2 ">
+            <div className="">
+              <div className="w-[140px] h-[140px] rounded-lg bg-white py-1">
+                <div className="circle m-3  rounded-full w-12 h-12 bg-blue-400">
+                  <p className="text-center font-bold text-4xl">M</p>
+                </div>
+                <div className=" text-blue-500 px-2 font-medium text-lg">
+                  ALL Menu
+                </div>
+                <div className=" px-2 text-gray-500">20 Items</div>
+              </div>
+            </div>
+
+            <div className="">
+              <div className="w-[140px] h-[140px] rounded-lg bg-white py-1">
+                <div className="circle m-3  rounded-full w-12 h-12 bg-blue-400">
+                  <p className="text-center font-bold text-4xl">M</p>
+                </div>
+                <div className=" text-blue-500 px-2 font-medium text-lg">
+                  ALL Menu
+                </div>
+                <div className=" px-2 text-gray-500">20 Items</div>
+              </div>
+            </div>
+
+            <div className="">
+              <div className="w-[140px] h-[140px] rounded-lg bg-white py-1">
+                <div className="circle m-3  rounded-full w-12 h-12 bg-blue-400">
+                  <p className="text-center font-bold text-4xl">M</p>
+                </div>
+                <div className=" text-blue-500 px-2 font-medium text-lg">
+                  ALL Menu
+                </div>
+                <div className=" px-2 text-gray-500">20 Items</div>
+              </div>
+            </div>
+
+            <div className="">
+              <div className="w-[140px] h-[140px] rounded-lg bg-white py-1">
+                <div className="circle m-3  rounded-full w-12 h-12 bg-blue-400">
+                  <p className="text-center font-bold text-4xl">M</p>
+                </div>
+                <div className=" text-blue-500 px-2 font-medium text-lg">
+                  ALL Menu
+                </div>
+                <div className=" px-2 text-gray-500">20 Items</div>
+              </div>
+            </div>
+
+            <div className="">
+              <div className="w-[140px] h-[140px] rounded-lg bg-white py-1">
+                <div className="circle m-3  rounded-full w-12 h-12 bg-blue-400">
+                  <p className="text-center font-bold text-4xl">M</p>
+                </div>
+                <div className=" text-blue-500 px-2 font-medium text-lg">
+                  ALL Menu
+                </div>
+                <div className=" px-2 text-gray-500">20 Items</div>
+              </div>
+            </div>
+            
+         
+          
+
+          
+
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MiniMenuNavbar;
