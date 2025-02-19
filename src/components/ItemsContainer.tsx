ef8891cdddee9d9e2cf26c6ab6a7e37dd814b169
Rajlodh-001 <Rajlodh911@gmail.com>
2025-02-19 21:13:10 +0530

    changes in sub manu

--- src/components/ItemsContainer.tsx
+++ src/components/ItemsContainer.tsx
@@ -1,13 +1,17 @@
+import MenuNavbar from "./MenuNavbar";
 import MenuPage from "./MenuPage"
 import MiniMenuNavbar from "./MiniMenuNavbar";
 import TopContainer from "./TopContainer";
+import SearchBox from "./SearchBox";
 
 const ItemsContainer = ()=>{
 
 return(
     <div className="h-screen bg-slate-300 w-full flex flex-col">
         <TopContainer/>
-        <MiniMenuNavbar/>
+        {/* <MiniMenuNavbar/> */}
+        <MenuNavbar/>
+        <SearchBox/>
         <MenuPage/>
     </div>
 )
