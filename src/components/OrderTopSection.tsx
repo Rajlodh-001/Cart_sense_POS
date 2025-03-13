2e9352a004770358cd5a661c1e0b919ed7936289
Rajlodh-001 <Rajlodh911@gmail.com>
2025-03-13 22:40:11 +0530

    upgraded drop down menu

--- src/components/OrderTopSection.tsx
+++ src/components/OrderTopSection.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import edit from "../../public/icons/edit.svg";
+import Image from "next/image";
+import DropDownMenu from "./DropDownMenu";
+
 
 const OrderTopSection = () => {
+
   return (
     <div className="  bg-white flex flex-col">
-
       <div className="top flex flex-row justify-between p-1 pt-2">
-
         <div className="bg-white rounded-full mx-2  h-12 w-12 flex justify-between items-center">
           <button className="bg-slate-200 rounded-full h-10 w-10 mx-1 font-bold">
             B
@@ -17,24 +21,23 @@ const OrderTopSection = () => {
           <span className="text-xs text-center">Order Number #0001</span>
         </div>
 
-         <div className="bg-white rounded-full mx-2  h-12 w-12 flex justify-between items-center">
+        <div className="bg-white rounded-full mx-2  h-12 w-12 flex justify-between items-center">
           <button className="bg-slate-200 rounded-full h-10 w-10 mx-1 font-bold">
             E
           </button>
         </div>
       </div>
 
-      <div className="flex flex-row justify-evenly p-2">
-        <div className="">
-            <label htmlFor="">Dine in</label>
-            
-        </div>
-        <div className="">
-          <label htmlFor="">Takeaway</label>
-          
-        </div>
-      </div>
+      <div className="grid grid-cols-2 gap-4 p-2 px-4 ">
+        {/* <div className="">
+          <label htmlFor="">Dine in</label>
+        </div> */}
 
+        <DropDownMenu/>
+
+        <DropDownMenu/>
+        
+      </div>
     </div>
   );
 };
